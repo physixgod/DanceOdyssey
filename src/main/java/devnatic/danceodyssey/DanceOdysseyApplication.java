4951dc7191c57db4022016f19c89987560374a75
physixgod <necroyasuo17@gmail.com>
2024-02-17 11:31:46 +0100

    first commit

---
+++ src/main/java/devnatic/danceodyssey/DanceOdysseyApplication.java
@@ -0,0 +1,13 @@
+package devnatic.danceodyssey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceOdysseyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanceOdysseyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/devnatic/danceodyssey/DanceOdysseyApplicationTests.java
@@ -0,0 +1,13 @@
+package devnatic.danceodyssey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceOdysseyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
