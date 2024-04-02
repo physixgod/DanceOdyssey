14d8c0ff560234c1076467cefe0b0bd05c608f7b
https://github.com/HOLLA2023 <oumyma.akrmi@esprit.tn>
2024-04-02 04:58:16 +0200

    verifier

---
+++ src/main/java/devnatic/danceodyssey/Services/DanceCoursesService.java
@@ -0,0 +1,50 @@
+package devnatic.danceodyssey.Services;
+
+import devnatic.danceodyssey.DAO.Entities.DanceCourses;
+import devnatic.danceodyssey.Repositories.DanceCoursesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DanceCoursesService {
+
+    @Autowired
+    private DanceCoursesRepository danceCoursesRepository;
+
+    // Opérations CRUD
+
+    public DanceCourses createDanceCourse(DanceCourses danceCourse) {
+        return danceCoursesRepository.save(danceCourse);
+    }
+
+    public Iterable<DanceCourses> getAllDanceCourses() {
+        return danceCoursesRepository.findAll();
+    }
+
+
+
+    public DanceCourses getDanceCourseById(int id) {
+        return danceCoursesRepository.findById(id).orElse(null);
+    }
+
+    public DanceCourses updateDanceCourse(DanceCourses danceCourse) {
+        return danceCoursesRepository.save(danceCourse);
+    }
+
+    public void deleteDanceCourseById(int id) {
+        danceCoursesRepository.deleteById(id);
+    }
+
+    // Méthodes de recherche personnalisées (optionnel)
+
+    public List<DanceCourses> findCoursesByCategory(String category) {
+        return danceCoursesRepository.findByCategory(category);
+    }
+
+    public List<DanceCourses> findCoursesByName(String name) {
+        return danceCoursesRepository.findByNameContaining(name);
+    }
+
+}
---
+++ src/main/java/devnatic/danceodyssey/Services/IDanceCoursesService.java
@@ -0,0 +1,19 @@
+package devnatic.danceodyssey.Services;
+
+import devnatic.danceodyssey.DAO.Entities.DanceCourses;
+
+import java.util.List;
+
+public interface IDanceCoursesService {
+
+    // CRUD operations
+    DanceCourses createDanceCourse(DanceCourses danceCourse);
+    List<DanceCourses> getAllDanceCourses();
+    DanceCourses getDanceCourseById(int id);
+    DanceCourses updateDanceCourse(DanceCourses danceCourse);
+    void deleteDanceCourseById(int id);
+
+    // Custom search methods (optional)
+    List<DanceCourses> findCoursesByCategory(String category);
+    List<DanceCourses> findCoursesByName(String name);
+}
